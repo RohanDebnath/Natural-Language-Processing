77651025b16e3a66d8ca63b79b016d737a157431
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-11-02 03:16:38 +0530

    Setup Done

--- nltkvid1.py
+++ nltkvid1.py
@@ -1,1 +1,13 @@
 import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize,word_tokenize
+"""
+Tokenizing - There are two kind of tokenizers
+            1> Word Tokenizers- Seperates by words.
+            1> Sentences Tokenizers- Seperates by sentences.
+
+Corpora- Body of texts. eg= Medical journals, English Language, Presidental Speeches
+lexion-  Words and their meanings.
+"""
+example_text="Hello Mr. Rohan Debnath. How are you today?"
+print(word_tokenize(example_text))
